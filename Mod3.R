b3a48842e5e8fa3fcaf3abccf0b50ca0afd33443
jforte2022 <107132180+jforte2022@users.noreply.github.com>
2023-01-23 12:08:01 -0500

    Add files via upload

---
+++ Mod3.R
@@ -0,0 +1,7 @@
+names <- c("Jeb", "Donald", "Ted", "Marco", "Carly", "Hillary", "Bernie")
+
+ABC <- c(4, 62, 51, 21, 2, 14, 15)
+
+CBS <- c(12, 75, 43, 19, 1, 21, 19)
+
+candidatePolling <- data.frame(names, ABC, CBS)
